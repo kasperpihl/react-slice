dba5bd9caefbfa011b383ad89b8e4088b9be6e1f
Kasper Pihl Tornøe <kasper@pihl.it>
2019-05-15 09:03:28 +0200

    master: exporting withSlice

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export { default as SliceProvider } from './SliceProvider';
 export { default as useSlice } from './useSlice';
 export { default as useSliceStore } from './useSliceStore';
 export { default as registerSlice } from './registerSlice';
+export { default as withSlice } from './withSlice';
