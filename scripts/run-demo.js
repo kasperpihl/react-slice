c1480d5f4f9c2dc6ce96530eed926ac081f8464d
Kasper Pihl Tornøe <kasper@pihl.it>
2019-07-01 08:41:06 +0200

    master: making demoscript cleanup after it self

--- scripts/run-demo.js
+++ scripts/run-demo.js
@@ -1,9 +1,13 @@
 const path = require('path');
+const fs = require('fs');
 const { execSync, spawn } = require('child_process');
 
 // ...
 const rootCwd = { cwd: path.resolve(__dirname, '../') };
 const demoCwd = { cwd: path.resolve(__dirname, '../demo') };
+const demoPackageJson = fs.readFileSync(
+  path.resolve(__dirname, '../demo/package.json')
+);
 
 console.log('--- STARTING DEMO BUILD ---');
 console.log('--- PACKING react-slice ---');
@@ -18,6 +22,15 @@ console.log('--- INSTALL DEMO DEPENDENCIES ---');
 execSync('npm i', demoCwd);
 console.log('--- INSTALL PACKAGE ---');
 execSync(`npm i ${rootCwd.cwd}/${fileName}`, demoCwd);
+console.log('--- REVERTING package.json ---');
+fs.writeFileSync(
+  path.resolve(__dirname, '../demo/package.json'),
+  demoPackageJson
+);
+console.log('--- DELETING package-lock.json ---');
+fs.unlinkSync(path.resolve(__dirname, '../demo/package-lock.json'));
+console.log('--- DELETING tarball ---');
+fs.unlinkSync(path.resolve(__dirname, '../', fileName));
 console.log('--- STARTING SERVER ---');
 
 // Notice how your arguments are in an array of strings
